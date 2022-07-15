9271e7cca5402e9ff43dc8996da95ab47243e9bf
Alberthua-Perl <hualongfeiyyy@163.com>
2022-07-16 00:21:08 +0800

    Add file comment

--- go-pro-ebook/chapter02/ftoc.go
+++ go-pro-ebook/chapter02/ftoc.go
@@ -4,10 +4,12 @@ import "fmt"
 
 func main() {
 	const freezingF, boilingF = 32.0, 212.0
+	// declare two const at the same time
 	fmt.Printf("%g F = %g C\n", freezingF, fToc(freezingF))
 	fmt.Printf("%g F = %g C\n", boilingF, fToc(boilingF))
 }
 
 func fToc(f float64) float64 {
 	return (f - 32) * 5 / 9
+	// use return keyword
 }
