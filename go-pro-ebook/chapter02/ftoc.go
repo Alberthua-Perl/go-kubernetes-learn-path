8624644a157562e1346652a997f9f0cd12e38820
Alberthua-Perl <hualongfeiyyy@163.com>
2022-07-17 00:49:59 +0800

    learn var declare

--- go-pro-ebook/chapter02/ftoc.go
+++ go-pro-ebook/chapter02/ftoc.go
@@ -7,6 +7,14 @@ func main() {
 	// declare two const at the same time
 	fmt.Printf("%g F = %g C\n", freezingF, fToc(freezingF))
 	fmt.Printf("%g F = %g C\n", boilingF, fToc(boilingF))
+
+	// just declare variable through type
+	var s string
+	fmt.Printf("Print vaule of s: \n", s)
+
+	// declare variable list through type or expression
+	var i, j, k int  // int, int, int
+	var b, g, p = true, 2.3, "four"  // bool, float64, string
 }
 
 func fToc(f float64) float64 {
