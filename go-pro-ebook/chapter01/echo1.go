4e8fe3fe775818d86f327107c8170a235718bf75
Alberthua-Perl <hualongfeiyyy@163.com>
2022-07-21 16:25:41 +0800

    Update echo versions

--- go-pro-ebook/chapter01/echo1.go
+++ go-pro-ebook/chapter01/echo1.go
@@ -8,8 +8,10 @@ import (
 func main() {
 	var s, sep string
 	for i := 1; i < len(os.Args); i++ {
+		// os.Args: slice type variable
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
+	// output final s variable including all cli arguements
 }
--- go-pro-ebook/chapter01/echo2.go
+++ go-pro-ebook/chapter01/echo2.go
@@ -8,8 +8,11 @@ import (
 func main() {
 	s, sep := "", ""
 	for _, arg := range os.Args[1:] {
+		// range return index and vaule of slice
+		// use "_" to omit assign of index
 		s += sep + arg
 		sep = " "
 	}
 	fmt.Println(s)
+	// output final s variable including all cli arguements
 }
