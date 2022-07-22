abfb135078c74bf58ee05fa0ec2fa6b654d3bc6d
Alberthua-Perl <hualongfeiyyy@163.com>
2022-07-23 00:32:11 +0800

    Learn go standard library function

--- go-pro-ebook/chapter02/echo4.go
+++ go-pro-ebook/chapter02/echo4.go
@@ -1,3 +1,4 @@
+// Note: pay attention to function return
 package main
 
 import (
@@ -6,13 +7,24 @@ import (
 	"strings"
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", " ", "separator")
+var n *bool = flag.Bool("n", false, "omit trailing newline")
+// flag.Bool: func Bool(name string, vaule bool, usage string) *bool
+var sep *string = flag.String("s", " ", "separator")
+// flag.String: func String(name string, vaule string, usage string) *string
+// variables for package level
+// access https://pkg.go.dev/flag@go1.16.6 to learn how to use flag package
 
 func main() {
+	// n := flag.Bool("n", false, "omit trailing newline")
+	// sep := flag.String("s", " ", "separator")
+	// local variables in function
 	flag.Parse()
 	fmt.Print(strings.Join(flag.Args(), *sep))
+	// no newline output
+	// fmt.Println(strings.Join(flag.Args(), *sep))
+	// flag.Args: func Args() []string
 	if !*n {
 		fmt.Println()
+		// output newline("\n") directly
 	}
 }
